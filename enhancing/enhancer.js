39d9112f6a4b7782fe34de3bca39ae8be49377de
AAsriyan <aasriyan@protonmail.com>
2019-04-08 16:43:01 -0400

    Added testing for repair, succeed, and fail

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,19 +2,54 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  const enhanced =
+    item.enhancement < 20 ? (item.enhancement += 1) : (item.enhancement = 20);
+  return { ...item, enhancement: enhanced };
 }
 
 function fail(item) {
-  return { ...item };
+  const dehanced = {
+    name: item.name,
+    durability: item.durability,
+    enhancement: item.enhancement
+  };
+
+  if (item.enhancement < 15) {
+    dehanced.durability -= 5;
+  } else if (item.enhancement >= 15) {
+    dehanced.durability -= 10;
+  }
+
+  if (dehanced.durability < 0) {
+    dehanced.durability = 0;
+  } else if (dehanced.durability > 100) {
+    dehanced.durability = 100;
+  }
+
+  if (item.enhancement < 0) {
+    dehanced.enhancement = 0;
+  } else if (item.enhancement > 20) {
+    dehanced.enhancement = 20;
+  } else if (item.enhancement > 16) {
+    dehanced.enhancement -= 1;
+  } else {
+    dehanced.enhancement;
+  }
+
+  return dehanced;
 }
 
 function repair(item) {
-  return { ...item };
+  newItem = {
+    name: item.name,
+    durability: 100,
+    enhancement: item.enhancement
+  };
+  return newItem;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,61 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("enhancement", () => {
+  describe("Repairing item", () => {
+    it("Should return an item with durability at 100", () => {
+      const itemRepair = {
+        name: "Sword",
+        durability: 80,
+        enhancement: 20
+      };
+
+      const repairedItem = {
+        name: "Sword",
+        durability: 100,
+        enhancement: 20
+      };
+
+      expect(enhancer.repair(itemRepair)).toEqual(repairedItem);
+    });
+  });
+
+  describe("Enhancing item", () => {
+    it("Should return an item with increased enchancement level", () => {
+      const itemEnhance = {
+        name: "Sword",
+        durability: 80,
+        enhancement: Math.floor(Math.random() * 10)
+      };
+      const successEnchance = {
+        name: "Sword",
+        durability: 80,
+        enhancement:
+          itemEnhance.enhancement < 20
+            ? itemEnhance.enhancement + 1
+            : (itemEnhance.enhancement = 20)
+      };
+      expect(enhancer.succeed(itemEnhance)).toEqual(successEnchance);
+    });
+
+    it("Should return an item with decreased enchancement level", () => {
+      const itemDehance = {
+        name: "Sword",
+        durability: 80,
+        enhancement: Math.floor(Math.random() * 10)
+      };
+      const failEnhance = {
+        name: "Sword",
+        durability:
+          itemDehance.enhancement < 15
+            ? itemDehance.durability - 5
+            : itemDehance.durability - 10,
+        enhancement:
+          itemDehance.enhancement > 16
+            ? itemDehance.enhancement - 1
+            : itemDehance.enhancement
+      };
+      expect(enhancer.fail(itemDehance)).toEqual(failEnhance);
+    });
+  });
+});
